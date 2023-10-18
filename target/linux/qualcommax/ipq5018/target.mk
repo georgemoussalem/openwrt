0c9720a0aab3cfcb4fddfa4170fdc837e522942e
George Moussalem <george.moussalem@outlook.com>
2023-10-18 20:05:38 +0400

    added switches and wifi

--- target/linux/qualcommax/ipq5018/target.mk
+++
@@ -1,7 +0,0 @@
-SUBTARGET:=ipq5018
-BOARDNAME:=Qualcomm Atheros IPQ5018
-DEFAULT_PACKAGES += ath11k-firmware-ipq5018
-
-define Target/Description
-	Build firmware images for Qualcomm Atheros IPQ5018 based boards.
-endef
